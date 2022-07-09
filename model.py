f5b04852ce4f7433ab81dd22a0aead21efd4b1e1
Andrew <92085084+kojomensahonums@users.noreply.github.com>
2022-07-09 01:06:16 +0000

    Update model.py

--- model.py
+++ model.py
@@ -7,25 +7,12 @@ from sklearn.datasets import fetch_california_housing
 housing= fetch_california_housing()
 X= pd.DataFrame(data= housing.data, columns= housing.feature_names)
 X.drop(columns=['MedInc', 'AveBedrms', 'Population', 'Latitude', 'Longitude'], inplace = True)
-X
 y= pd.DataFrame(data= housing.target)
-#y
-0
-0	4.526
-1	3.585
-2	3.521
-3	3.413
-4	3.422
-...	...
-20635	0.781
-20636	0.771
-20637	0.923
-20638	0.847
-20639	0.894
-20640 rows × 1 columns
+
 
 X_train, X_test, y_train, y_test= train_test_split(X, y, test_size= 0.3, random_state= 100)
 lm= LinearRegression()
 lm.fit(X_train, y_train)
-LinearRegression()
-pickle.dump(lm, open('modell.pickle', 'wb'))
+#LinearRegression()
+s = pickle.dumps(lm)
+pickle.dump(s, open('model.pickle', 'wb'))
